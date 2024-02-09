1977353aea7d90fd36086db853ccee736f799b83
Markus Kerle <m.kerle@jeanmueller.de>
2024-02-09 22:49:33 +0100

    store thoughts

--- sml/receive_sml_post.php
+++ sml/receive_sml_post.php
@@ -19,8 +19,18 @@ if('XXXXXXXX' <> $token) {
 }
     
 //$sql = "INSERT INTO data_object (title, name, address, contact, fk_object) VALUES ('${title}', '${name}', '${address}', '${contact}', '${parent}')";
-
 //$result = query($conn, $sql);
+
+/*
+
+only log every 10 seconds
+
+prune concept:
+keep the last week.
+delete all except one per hour for the last 4 weeks
+delete all except one per day for the restore_error_handler
+
+*/
 
 $myjson = json_decode(file_get_contents("php://input"), true);
 $myjsondata = json_decode($myjson);
