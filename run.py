3302e6e238bdee95adee7b7afdbe7d7df7ba5110
Markus Kerle <m.kerle@jeanmueller.de>
2024-02-09 21:02:02 +0100

    parse data to dict

--- run.py
+++ run.py
@@ -1,22 +1,29 @@
- import sys
+import sys
+import json
+
+single_data = {}
+
 while 1:
     line = sys.stdin.readline()
     if not line:
         break
     first = line.split(":")
     medium_kanal = first[0]
-    print(medium_kanal)
     if(len(first)>1):
         second = first[1].split("*",1)
-        messgroesse_messart_tarifstufe = second[0]
+        messgroesse_messart_tarifstufe = second[0].replace(".","_")
         wert = second[1]
-        print(messgroesse_messart_tarifstufe)
         wert_roh = wert.replace("(","").replace(")","")
         wert_roh_a = wert_roh.split("*")
         wert_zahl = wert_roh_a[0]
-        print(wert_zahl)
+        single_data[messgroesse_messart_tarifstufe] = wert_zahl
         if(len(wert_roh_a)>1):
             wert_einheit = wert_roh_a[1]
-            print(wert_einheit)
     if 'DE1234560000000000000001298898157' == line.rstrip():
-        print('found end')
+        data_sml = {}
+        data_sml['Time'] = 'mytime'
+        data_sml['SML'] = single_data
+        data = {}
+        data['StatusSNS'] = data_sml
+        print(data)
+        single_data = {}
