7b72b49a221e2355b23638382a3df3da6bb86a6b
Markus Kerle <m.kerle@jeanmueller.de>
2024-02-16 22:48:02 +0100

    add more values to database

--- sml/receive_sml_post.php
+++ sml/receive_sml_post.php
@@ -21,17 +21,20 @@ if('XXXXXXXX' <> $token) {
 $myjson = json_decode(file_get_contents("php://input"), true);
 $myjsondata = json_decode($myjson);
 
-$sql = "SELECT 1_8_0 FROM power ORDER BY time DESC LIMIT 1";
+$sql = "SELECT * FROM power ORDER BY time DESC LIMIT 1";
 $result = query($conn, $sql);
 $row = $result->fetch_assoc();
 $last_value = $row["1_8_0"];
+$last_time = $row["time"];
 
 $time = $myjsondata->StatusSNS->Time;
 $time = str_replace("T", " ", $time);
 $value = $myjsondata->StatusSNS->SML->{'1_8_0'};
-$difference = $value - $last_value;
+$diff_energy = $value - $last_value;
+$diff_time = strtotime($time) - strtotime($last_time);
+$energy = ($diff_energy * 3600 * 1000) / $diff_time;
 
-$sql = "INSERT INTO power (time, 1_8_0, difference) VALUES ('${time}', '${value}', '${difference}')";
+$sql = "INSERT INTO power (time, 1_8_0, diff_energy, diff_time, energy) VALUES ('${time}', '${value}', '${diff_energy}', '${diff_time}', '${energy}')";
 $result = query($conn, $sql);
 
 $error = error($conn);
@@ -41,6 +44,6 @@ if("" <> $error) {
 }
 
 echo $myjsondata->StatusSNS->Time . " - " . $myjsondata->StatusSNS->SML->{'1_8_0'} . " - " . $myjsondata->StatusSNS->SML->{'1_7_255'} . "\r\n";
-echo "updated: " . $time . " - " . $value . " - " . $difference . "\r\n";
+echo "updated: " . $time . " - " . $value . " - " . $diff_energy . " - " . $diff_time . " - " . $energy . "\r\n";
 
 ?>
