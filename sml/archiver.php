73393278fcec37b981354364edf79b41f79a4ee0
Markus Kerle <m.kerle@jeanmueller.de>
2024-02-21 07:05:16 +0100

    move purge to class

--- sml/archiver.php
+++ sml/archiver.php
@@ -105,7 +105,70 @@ class MyArchiverAPI {
     }
           
     return $return_result;
-  }  
+  }
+  
+  function purgeDB() {
+/*
+
+purge concept: (separate script)
+keep the last week.
+delete all except one per hour for the last 4 weeks
+delete all except one per day for the restore_error_handler
+
+age
+0-7     all
+7-28    hourly
+>28     daily
+
+--data sets to keep:
+SELECT max(time), max(id), DATE_FORMAT(time, '%d-%m-%Y %H:%i') 
+FROM `power` 
+WHERE time < DATE_SUB(NOW(), INTERVAL 2 DAY)
+AND time > DATE_SUB(NOW(), INTERVAL 4 DAY)
+GROUP by DATE_FORMAT(time, '%d-%m-%Y %H')
+
+-- delete sets:
+-- DELETE
+SELECT *
+FROM power
+WHERE time < DATE_SUB(NOW(), INTERVAL 2 DAY)
+AND time > DATE_SUB(NOW(), INTERVAL 4 DAY)
+AND id NOT IN (SELECT min(id)
+    FROM `power` 
+    WHERE time < DATE_SUB(NOW(), INTERVAL 2 DAY)
+    AND time > DATE_SUB(NOW(), INTERVAL 4 DAY)
+    GROUP by DATE_FORMAT(time, '%d-%m-%Y %H'))
+
+
+--data sets to keep:
+SELECT max(time), max(id), DATE_FORMAT(time, '%d-%m-%Y %H:%i') 
+FROM `power` 
+WHERE time < DATE_SUB(NOW(), INTERVAL 4 DAY)
+GROUP by DATE_FORMAT(time, '%d-%m-%Y %H')
+
+-- delete sets:
+-- DELETE 
+SELECT *
+FROM power 
+WHERE time < DATE_SUB(NOW(), INTERVAL 4 DAY)
+AND id NOT IN (SELECT min(id)
+    FROM `power` 
+    WHERE time < DATE_SUB(NOW(), INTERVAL 4 DAY)
+    GROUP by DATE_FORMAT(time, '%d-%m-%Y'))
+
+SELECT * 
+FROM power
+WHERE time < DATE_SUB(NOW(), INTERVAL 2 DAY)
+    AND time > DATE_SUB(NOW(), INTERVAL 5 DAY)
+   ORDER BY time
+    
+UPDATE power
+set energy = ?
+where id = ?  
+  
+    recalc all values between 2 and 5 days
+*/    
+  }
 }
 
 ?>
--- sml/purge.php
+++ sml/purge.php
@@ -1,64 +1,20 @@
 <?PHP
 
-/*
+require("config.php");
+require("archiver.php");
 
-prune concept: (separate script)
-keep the last week.
-delete all except one per hour for the last 4 weeks
-delete all except one per day for the restore_error_handler
+$token = get_control($_REQUEST, "token", NULL);
 
-age
-0-7     all
-7-28    hourly
->28     daily
+if($secret_token <> $token) {
+  echo "error: not authenticated";
+  die();
+}
 
---data sets to keep:
-SELECT max(time), max(id), DATE_FORMAT(time, '%d-%m-%Y %H:%i') 
-FROM `power` 
-WHERE time < DATE_SUB(NOW(), INTERVAL 2 DAY)
-AND time > DATE_SUB(NOW(), INTERVAL 4 DAY)
-GROUP by DATE_FORMAT(time, '%d-%m-%Y %H')
+$myAPI = new MyArchiverAPI();
+$myAPI->set_db_connection($dbhost, $dbuser, $dbpass, $dbname);
 
--- delete sets:
--- DELETE
-SELECT *
-FROM power
-WHERE time < DATE_SUB(NOW(), INTERVAL 2 DAY)
-AND time > DATE_SUB(NOW(), INTERVAL 4 DAY)
-AND id NOT IN (SELECT min(id)
-    FROM `power` 
-    WHERE time < DATE_SUB(NOW(), INTERVAL 2 DAY)
-    AND time > DATE_SUB(NOW(), INTERVAL 4 DAY)
-    GROUP by DATE_FORMAT(time, '%d-%m-%Y %H'))
+$result = $myAPI->purgeDB();
 
-
---data sets to keep:
-SELECT max(time), max(id), DATE_FORMAT(time, '%d-%m-%Y %H:%i') 
-FROM `power` 
-WHERE time < DATE_SUB(NOW(), INTERVAL 4 DAY)
-GROUP by DATE_FORMAT(time, '%d-%m-%Y %H')
-
--- delete sets:
--- DELETE 
-SELECT *
-FROM power 
-WHERE time < DATE_SUB(NOW(), INTERVAL 4 DAY)
-AND id NOT IN (SELECT min(id)
-    FROM `power` 
-    WHERE time < DATE_SUB(NOW(), INTERVAL 4 DAY)
-    GROUP by DATE_FORMAT(time, '%d-%m-%Y'))
-
-SELECT * 
-FROM power
-WHERE time < DATE_SUB(NOW(), INTERVAL 2 DAY)
-    AND time > DATE_SUB(NOW(), INTERVAL 5 DAY)
-   ORDER BY time
-    
-UPDATE power
-set energy = ?
-where id = ?  
-  
-    recalc all values between 2 and 5 days
-*/
+echo $result;
 
 ?>
